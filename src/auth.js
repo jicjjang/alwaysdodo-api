5e39a53efb3f9d3080da02e91fd6c61948006a0b
moka-a <aud1220a@gmail.com>
2018-09-29 05:49:52 +0900

    bug-fix

--- src/auth.js
+++ src/auth.js
@@ -2,16 +2,15 @@ const helper = require("./helper")
 const jwt = require('jsonwebtoken')
 
 const authMiddleware = (req, res, next) => {
-    const token = req.headers['x-access-token'] || req.query.token
-
+    const token = (req.headers['Authorization'] || "").split("Bearer ")[1]
     if (!token) {
-        helper.sendTokenFail(req, res)
+        helper.sendTokenFail(res)
         return
     }
 
     jwt.verify(token, req.app.get('jwt-secret'), (err, decoded) => {
         if (err) {
-            helper.sendTokenFail(req, res)
+            helper.sendTokenFail(res)
         } else {
             req.decoded = decoded
             next()
