e997787dad6c5acd9edaab6b18b30478e57e9c15
moka-a <aud1220a@gmail.com>
2018-09-29 05:37:31 +0900

    add cors

--- src/entry.js
+++ src/entry.js
@@ -3,10 +3,12 @@ require('dotenv').config(process.cwd())
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.set('jwt-secret', process.env.JWT_SECRET)
 
